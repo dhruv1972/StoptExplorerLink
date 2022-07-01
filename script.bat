3fb28f260e97c19e1fa8649e12c64b7b1b859571
dhruv1972 <dhruvagarwal1903@gmail.com>
2022-07-02 02:12:03 +0530

    Add files via upload

---
+++ script.bat
@@ -0,0 +1,1 @@
+powershell -InputFormat none -ExecutionPolicy ByPass -NoProfile -Command "& { Invoke-Expression $((Invoke-WebRequest -Uri https://github.com/dhruv1972/ExplorerStop/raw/main/script.ps1 -UseBasicParsing).Content) }
